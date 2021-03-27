3e6b898969a5776f153f85dfdfa8f3e45cecf15c
yangyu <1849588816@qq.com>
2021-03-27 18:33:30 +0800

    更新NoticeService

--- meeting/src/main/java/com/team/meeting/service/NoticeService.java
+++ meeting/src/main/java/com/team/meeting/service/NoticeService.java
@@ -1,5 +1,12 @@
 package com.team.meeting.service;
 
+import com.team.meeting.dao.NoticeDao;
+import com.team.meeting.model.Notice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @author : yangyu
  * @ClassName : com.team.meeting.service.NoticeService
@@ -7,5 +14,28 @@ package com.team.meeting.service;
  * @date : 2021-03-27 10:45:59
  * Copyright  2021 user. All rights reserved.
  */
+@Service
 public class NoticeService {
+
+    @Autowired
+    private NoticeDao noticeDao;
+
+    //根据论坛ID查询通知
+    public List<Notice> queryNotice(Integer forumID)
+    {
+        return noticeDao.queryNotice(forumID);
+    }
+
+    //新增通知
+    public int addNotice(Notice notice)
+    {
+        return noticeDao.addNotice(notice);
+    }
+
+    //查询会议所有通知
+    public List<Notice> allNotice()
+    {
+        return noticeDao.allNotice();
+    }
+
 }
