d9231e8a752bc4e864fba7e554370c16440f4423
yangyu <1849588816@qq.com>
2021-03-27 11:43:07 +0800

    新增接NoticeService类

---
+++ meeting/src/main/java/com/team/meeting/service/NoticeService.java
@@ -0,0 +1,11 @@
+package com.team.meeting.service;
+
+/**
+ * @author : yangyu
+ * @ClassName : com.team.meeting.service.NoticeService
+ * @Description : 类描述
+ * @date : 2021-03-27 10:45:59
+ * Copyright  2021 user. All rights reserved.
+ */
+public class NoticeService {
+}
