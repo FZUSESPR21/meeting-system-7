352036195ee534b8b2313aa06d0ba280c636ae69
Fzuerzmj <794858610@qq.com>
2021-03-27 15:39:27 +0800

    221801222（add）: index界面布局

---
+++ frontend/meeting_system/src/views/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="index">
+
+    <el-container>
+<!--      /*左侧边栏*/-->
+      <el-aside width="200px">
+        <MyMenu></MyMenu>
+      </el-aside>
+      <el-container>
+<!--        /*头部*/-->
+        <el-header>
+
+        </el-header>
+<!--        /* 主体 */-->
+        <el-main>
+          Main
+        </el-main>
+<!--        /*页尾*/-->
+        <el-footer>
+          Footer
+        </el-footer>
+<!--        /*右侧边栏*/-->
+      </el-container>
+      <el-aside width="200px">
+        <MessageList></MessageList>
+      </el-aside>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import MessageList from "../components/message-list"
+import MyMenu from "../components/mymenu";
+
+export default {
+  name: 'Index',
+  components: {MessageList, MyMenu}
+}
+</script>
+
+<style>
+.el-header, .el-footer {
+  background-color: aliceblue;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  /*background-color: #D3DCE6;*/
+  /*color: #333;*/
+  /*text-align: center;*/
+  /*line-height: 200px;*/
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
