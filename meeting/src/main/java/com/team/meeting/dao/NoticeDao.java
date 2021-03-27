04369e4a5952bcbf45b317fbe96b5ca078d49dc8
yangyu <1849588816@qq.com>
2021-03-27 11:40:06 +0800

    新增NoticeDao接口

---
+++ meeting/src/main/java/com/team/meeting/dao/NoticeDao.java
@@ -0,0 +1,11 @@
+package com.team.meeting.dao;
+
+/**
+ * @author : yangyu
+ * @ClassName : com.team.meeting.dao.NoticeDao
+ * @Description : 类描述
+ * @date : 2021-03-27 10:44:53
+ * Copyright  2021 user. All rights reserved.
+ */
+public interface NoticeDao {
+}
