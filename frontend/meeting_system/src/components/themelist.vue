a63d6dced7ef1897c2b870c6ddf730f8b2461d73
YuhanTse <60552555+YuhanTse@users.noreply.github.com>
2021-03-27 16:39:38 +0800

    与会人员和议会列表
    与会人员和议会列表

---
+++ frontend/meeting_system/src/components/themelist.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      fixed
+      prop="id"
+      label="会议编号"
+      width="400">
+    </el-table-column>
+    <el-table-column
+      prop="theme"
+      label="会议主题"
+      width="400">
+    </el-table-column>
+    <el-table-column
+      prop="start_time"
+      label="开始时间"
+      width="400">
+    </el-table-column>
+    <el-table-column
+      fixed="right"
+      label="操作"
+      width="150">
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleClick (row) {
+        console.log(row)
+      }
+    },
+
+    data () {
+      return {
+        tableData: [{
+          id: '001',
+          theme: 'xxx',
+          start_time: 'xxx'
+        }]
+      }
+    }
+  }
+</script>
+
